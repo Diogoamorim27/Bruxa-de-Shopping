7b0cde7db23803b64f0a96323463c13d016d9530
Diogoamorim27 <diogoamorim27@gmail.com>
2019-08-17 11:19:47 -0300

    started implementing inventory drag n drop system

---
+++ ItemDB.gd
@@ -0,0 +1,28 @@
+extends Node
+
+const ICON_PATH = "res://Assets/Sprites/Craftables/"
+
+const ITEMS = {
+	"wrinkled_paper": {
+		"icon": ICON_PATH + "paper.pgn",
+		"category": "lightness"
+	},
+	"plastic_bottle": {
+		"icon": ICON_PATH + "bottle.png",
+		"category": "lightness"
+	},
+	"cola" : {
+		"icon": ICON_PATH + "cola.png",
+		"category": "soda"
+	},
+	"fries": {
+		"icon": ICON_PATH + "fries.png",
+		"category" : "grease"
+	}	
+}
+
+func get_item(item_id):
+    if item_id in ITEMS:
+        return ITEMS[item_id]
+    else:
+        return ITEMS["error"]	
