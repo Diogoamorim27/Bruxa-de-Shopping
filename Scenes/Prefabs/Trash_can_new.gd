746f72eac56c9ebb21379739c7fa7eac4dd9868b
Diogoamorim27 <diogoamorim27@gmail.com>
2019-09-04 22:06:39 -0300

    "?" appearing

--- Scenes/Prefabs/Trash_can_new.gd
+++ Scenes/Prefabs/Trash_can_new.gd
@@ -13,10 +13,16 @@ func _ready():
 	items = [item_1, item_2, item_3]
 	print(items)
 
+func _process(delta):
+	$AttentionSprite.visible = false
+	for body in get_overlapping_bodies():
+		if body.name == "Player":
+			$AttentionSprite.visible = true
+
 func _on_Trashcan_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		get_tree().call_group("dialogue_panel", "_trashcan_opened")
 	for body in get_overlapping_bodies():
 		if body.name == "Player":
 			if event is InputEventMouseButton:
 				current_trashcan = true
+				get_tree().call_group("dialogue_panel", "_trashcan_opened")
+
